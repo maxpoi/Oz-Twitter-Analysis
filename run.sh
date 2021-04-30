58cce5dd7972a99a3f55a904760cd219eec86d55
Jiacheng Ye <jiachengy1@student.unimelb.edu.au>
2021-04-30 18:05:49 +0800

    fix bugs & tested that main.yaml can be run successfully & multiple instances can be created & docker can be installed; TODO: deploy couchdb nodes & make a cluster

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-ansible-playbook ./ansible/uninstall_server.yaml; ansible-playbook ./ansible/main.yaml
+# . ./openrc.sh
+#ansible-playbook -i ./ansible/hosts ./ansible/uninstall_server.yaml
+ansible-playbook -i ./ansible/hosts ./ansible/main.yaml
