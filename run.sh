dd5e3f57726156ff9a9d6413626f9504861bb159
Jiacheng Ye <jiachengy1@student.unimelb.edu.au>
2021-04-30 23:46:59 +0800

    I am crying..; finally docker is working correctly on each node; couchdb is started, but due to some port issues (maybe) I cannot access it, so I cannot test it

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 . ./openrc.sh
-ansible-playbook -i ./ansible/hosts ./ansible/uninstall_server.yaml
+# ansible-playbook -i ./ansible/hosts ./ansible/uninstall_server.yaml
 ansible-playbook -i ./ansible/hosts ./ansible/main.yaml
