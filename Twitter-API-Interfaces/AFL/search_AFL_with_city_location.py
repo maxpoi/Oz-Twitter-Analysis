5fc8f0d47283c947c38383651852a2784071d0dc
Ma-Yuyao <antyaoyao@gmail.com>
2021-05-06 11:04:24 +0800

    Changed the json file format

--- Twitter-API-Interfaces/AFL/search_AFL_with_city_location.py
+++ Twitter-API-Interfaces/AFL/search_AFL_with_city_location.py
@@ -17,8 +17,6 @@ city_geo = {'Melbourne': '-37.813611,144.963056,60km', 'Sydney': '-33.865143,151
 # 查询出现在某个 city 中的所有 带有 keyword 的推文数量
 # return 统计得到的数字
 def count_keyword_in_different_cities(keyword, city, max_num, api):
-    filename = keyword + '_twitter_data.json'
-    file_handle=open(filename,mode='a')
     result_list = []
 
     count = 0
@@ -38,8 +36,8 @@ def count_keyword_in_different_cities(keyword, city, max_num, api):
         # 如果查询结果为空，则结束循环
         if (len(search_results) == 0):
             # dict_result = {'keyword': keyword, 'city': city, 'results':result_list}
-            json.dump(result_list, file_handle)
-            return count
+            print(city, count)
+            return result_list
 
         # Update max_id --- max_id 是最后一条查询结果的 _id
         max_id = search_results[-1]._json['id'] - 1
@@ -49,8 +47,8 @@ def count_keyword_in_different_cities(keyword, city, max_num, api):
             # 如果这条的 id 和 上一条一样，证明已经找到了所有 tweets, return 即可
             if (tweet._json['id'] == prev_id):
                 # dict_result = {'keyword': keyword, 'city': city, 'results':result_list}
-                json.dump(result_list, file_handle)
-                return count
+                print(city, count)
+                return result_list
 
             # tweet 还是一个对象,推特的相关信息在 tweer._json 里
             #这里是检测消息是否含有'text'键,并不是所有 TWitter 返回的所有对象都是消息(有些可能是用来删除消息或者其他内容的动作--这个没有确认),区别就是消息对象中是否含有'text'键
@@ -58,11 +56,11 @@ def count_keyword_in_different_cities(keyword, city, max_num, api):
                 count += 1
                 dict_search = {'id': tweet._json['id_str'], 'created': tweet._json['created_at'], 'text': tweet._json['text'], 'city': city}
                 result_list.append(dict_search)
-                # print(result_list)
-                # file_handle.write('{\"id\":' + str(tweet._json['id_str']) + ',' + '\"created\":' + str(tweet._json['created_at']) + ',' + '\"text\":' + str(tweet._json['text']) + '},')
+                    # print(result_list)
+                    # file_handle.write('{\"id\":' + str(tweet._json['id_str']) + ',' + '\"created\":' + str(tweet._json['created_at']) + ',' + '\"text\":' + str(tweet._json['text']) + '},')
                 prev_id = tweet._json['id']
-
-    return count
+    print(city, count)
+    return result_list
 
 def main():
     # 得到 Twitter API 句柄
@@ -74,12 +72,16 @@ def main():
     # 定义最大查询 twitter 数量
     twitter_max_num = 100000
 
-    # 想要查询的城市
-    # city = 'Canberra'
+    filename = search_keyword + '_twitter_data.json'
+
+    file_handle=open(filename,mode='w')
+
+    result_list = []
 
     for city in city_geo:
-        count = count_keyword_in_different_cities(search_keyword, city, twitter_max_num, api)
-        print (count)
+        result_list.extend(count_keyword_in_different_cities(search_keyword, city, twitter_max_num, api))
+
+    json.dump(result_list, file_handle)
 
 if __name__ == '__main__':
     main()
--- Twitter-API-Interfaces/food/search_food_with_city_location.py
+++ Twitter-API-Interfaces/food/search_food_with_city_location.py
@@ -17,8 +17,7 @@ city_geo = {'Melbourne': '-37.813611,144.963056,60km', 'Sydney': '-33.865143,151
 # 查询出现在某个 city 中的所有 带有 keyword 的推文数量
 # return 统计得到的数字
 def count_keyword_in_different_cities(keyword, city, max_num, api):
-    filename = 'food_twitter_data.json'
-    file_handle=open(filename,mode='a')
+
     result_list = []
 
     if (keyword == '(Chinese food) OR (Dim sum) OR (dumpling) OR (Chow Mein) OR (fried rice) OR (hot pot)'):
@@ -51,8 +50,7 @@ def count_keyword_in_different_cities(keyword, city, max_num, api):
         # 如果查询结果为空，则结束循环
         if (len(search_results) == 0):
             # dict_result = {'keyword': keyword, 'city': city, 'results':result_list}
-            json.dump(result_list, file_handle)
-            return count
+            return result_list
 
         # Update max_id --- max_id 是最后一条查询结果的 _id
         max_id = search_results[-1]._json['id'] - 1
@@ -62,8 +60,8 @@ def count_keyword_in_different_cities(keyword, city, max_num, api):
             # 如果这条的 id 和 上一条一样，证明已经找到了所有 tweets, return 即可
             if (tweet._json['id'] == prev_id):
                 # dict_result = {'keyword': keyword, 'city': city, 'results':result_list}
-                json.dump(result_list, file_handle)
-                return count
+                print(count)
+                return result_list
 
             # tweet 还是一个对象,推特的相关信息在 tweer._json 里
             #这里是检测消息是否含有'text'键,并不是所有 TWitter 返回的所有对象都是消息(有些可能是用来删除消息或者其他内容的动作--这个没有确认),区别就是消息对象中是否含有'text'键
@@ -81,20 +79,25 @@ def main():
     # 得到 Twitter API 句柄
     api = twitter_api()
 
-    # The keyword we would like to search
-    search_keyword = 'AFL'
-
     # 定义最大查询 twitter 数量
     twitter_max_num = 100000
+
+    filename = 'food_twitter_data.json'
+
+    file_handle=open(filename,mode='w')
+
+    result_list = []
 
     # 共查询六种菜系：
     food_keyword = ['(Chinese food) OR (Dim sum) OR (dumpling) OR (Chow Mein) OR (fried rice) OR (hot pot)', '(American food) OR (hamburger) OR (French fries) OR (KFC) OR (McDonald\'s)', 
                     '(Italian food) OR (pasta) OR (pizza) OR (spaghetti)', '(Korean food) OR (bibimbap) OR (kimchi)', '(Japanese food) OR (sushi) OR (sashimi)', 
                     '(French food) OR (baguette) OR (croissant) OR (macaron)']
+    
     for city in city_geo:
         for food in food_keyword:
-            count = count_keyword_in_different_cities(food, city, twitter_max_num, api)
-            print (city, food, count)
+            result_list.append(count_keyword_in_different_cities(food, city, twitter_max_num, api))
+
+    json.dump(result_list, file_handle)
 
 if __name__ == '__main__':
     main()
