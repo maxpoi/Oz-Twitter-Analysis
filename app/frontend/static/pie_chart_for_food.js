7b760f36a5585655d4e8ef81ad46f3aeffe5c3d9
Shiyi Xu <xus3@student.unimelb.edu.au>
2021-05-22 17:43:45 +1000

    scene4 v1

--- app/frontend/static/pie_chart_for_food.js
+++ app/frontend/static/pie_chart_for_food.js
@@ -1,9 +1,13 @@
-
+let food_data_pie = [['Attitude', 'TwitterCount']];
 
 function myFunc(data, city) {
-  food_data_pie = [['Attitude', 'TwitterCount']];
-  element_id = "piechart_food_" + city.toLowerCase()
   data = JSON.parse(data)
+  us_food = 0
+  cn_food = 0
+  fr_food = 0
+  it_food = 0
+  jp_food = 0
+  kr_food = 0
   for (let i = 0; i < data.length; i++) {
     if (city === data[i].key[0]){
         food_data_pie.push(
@@ -21,6 +25,6 @@ function drawChart(food_data_pie, element_id, city) {
     // pieHole: 0.4,
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById(element_id));
+  var chart = new google.visualization.PieChart(document.getElementById('piechart_food_overall'));
   chart.draw(figure_data, options);
 }
