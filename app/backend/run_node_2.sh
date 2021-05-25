26b3e32c1a6a4b14a1b0fa208aa9edce86347573
Jiacheng Ye <jiachengy1@student.unimelb.edu.au>
2021-05-25 19:38:35 +0800

    demo fix

--- app/backend/run_node_2.sh
+++ app/backend/run_node_2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t harvester -f crawler/crawl_raw_data/node_2/Dockerfile .
-docker run -d -p 8001:8001 harvester --name="harvester"
+docker run -d -p 8001:8001 --name harvester harvester
--- app/backend/run_upload_data.sh
+++ app/backend/run_upload_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t upload_data -f upload_data/Dockerfile . 
-docker run -d -p 8002:8002 upload_data --name="upload_data"
+docker run -d -p 8002:8002 --name upload_data upload_data
